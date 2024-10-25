d521be701ad8ef26b5c4ea671724b2c66ca2afec
Liam0018 <Liam.magnusson@bellevuecollege.edu>
2024-10-25 16:23:11 -0700

    Update bullseye.js

--- m05/js/bullseye.js
+++ m05/js/bullseye.js
@@ -1,24 +1,23 @@
 
 var table = prompt("Please enter a multiplication table", "1");             // Unit of table
-var operator = 'addition'; // Type of calculation
+// var operator = 'addition'; // Type of calculation
 var i = 1;                 // Set counter to 1
 var msg = '<h2>Multiplication Table</h2>';            // Message
 
-if (operator === 'addition') {
+/* if (operator === 'addition') {
   // Do addition
   while (i < 11) {
     msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
     i++;
   }
-} else {
+} else { */
   // Do multiplication
   while (i < 11) {
     msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
     i++;
   }
-}
+// }
 
 // Write the message into the page
-// let table = prompt("Please enter a multiplication table", "1");
 var el = document.getElementById('blackboard');
 el.innerHTML = msg;
