9cc6b73b9b85805ac49b8faec9a70adda5ce0523
Liam0018 <Liam.magnusson@bellevuecollege.edu>
2024-11-04 16:46:28 -0800

    Update java.js

--- a3/java.js
+++ a3/java.js
@@ -4,9 +4,9 @@
 // var symbol ="*";
 
 function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
+upLeft(pHeight, pColorEven, pColorOdd, pSymbol);
 upRight(pHeight, pColorEven, pColorOdd, pSymbol);
 downRight(pHeight, pColorEven, pColorOdd, pSymbol);
-
 }
 
 function upRight(pHeight, pColorEven, pColorOdd, pSymbol){
@@ -55,4 +55,31 @@ rLine +="</p>";
 }
 
 document.getElementById("downRight").innerHTML = rLine;
+}
+
+function upLeft(pHeight, pColorEven, pColorOdd, pSymbol){
+var rLine ="";
+for (i=0;i<pHeight;i++){
+rLine +="<p>";
+for (k=0;k<(pHeight-i);k++){
+rLine +="<span style='color: #ffffff;'>*</span>"
+}
+//Create each line on the Rhombus
+for(j=0;j<=i;j++){
+
+//Is the position even or odd so we change the color
+if (j%2)
+//even
+rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
+else
+//odd
+rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+
+}
+rLine +="</p>";
+// console.log(rLine);
+
+}
+
+document.getElementById("upLeft").innerHTML = rLine;
 }
